e5fbaf1542e57a46f259eaeb94fe8b325811b844
Anastasiia <kalinovskaya.anastasia@gmail.com>
2019-12-24 23:57:46 +0200

    fixed errors

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 // Your code goes here
 
 
@@ -5,16 +6,16 @@ let a = prompt('Enter the coefficients for the quadratic equation (ax ^ 2 + bx +
 let b = prompt('Enter the coefficients for the quadratic equation (ax ^ 2 + bx + c = 0)  b =');
 let c = prompt('Enter the coefficients for the quadratic equation (ax ^ 2 + bx + c = 0)  c =');
 
-let discr = (b * b) - (4 * a * c);
-console.log("Discriminant has value of :" + discr);
+let discr = b * b - 4 * a * c;
+console.log('Discriminant has value of :' + discr);
 
 if (a === 0 || (isNaN(a) || isNaN(b) || isNaN(c))) {
-    console.log("Invalid input data");
+    console.log('Invalid input data');
 } else if (discr >= 0) {
     let x1 = (-b + Math.sqrt(discr)) / (2 * a);
     let x2 = (-b - Math.sqrt(discr)) / (2 * a);
     console.log('first value: ' + x1);
     console.log('second value: ' + x2);
 } else {
-    console.log("there is no solution");
+    console.log('there is no solution');
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,21 +1,21 @@
-let a = prompt("Enter first side of triangle");
-let b = prompt("Enter second side of triangle");
-let c = prompt("Enter third side of triangle");
+let a = prompt('Enter first side of triangle');
+let b = prompt('Enter second side of triangle');
+let c = prompt('Enter third side of triangle');
 
-if ((a === '' || undefined) || (b === '' || undefined) || (c === '' || undefined)) {
+if (a === '' || undefined || (b === '' || undefined) || (c === '' || undefined)) {
     alert('input values should be ONLY numbers ');
-} else if ((a == 0) || (b == 0) || (c == 0)) {
+} else if (a === 0 || b === 0 || c === 0) {
     alert('A triangle must have 3 sides with a positive definite length ');
 } else if (a && b && c > 0) {
-    alert('sides of triangle: ' + a + ", " + b + ", " + c);
+    alert('sides of triangle: ' + a + ', ' + b + ', ' + c);
 }
 
-if (a == b && b == c) {
-    console.log("Equilateral triangle");
-} else if (a == b || a == c) {
+if (a === b && b === c) {
+    console.log('Equilateral triangle');
+} else if (a === b || a === c) {
     console.log('Isosceles triangle');
-} else if (a != b && a != c && b != c) {
+} else if (a !== b && a !== c && b !== c) {
     console.log('Scalene triangle');
 } else {
-    console.log("Triangle doesn’t exist");
+    console.log('Triangle doesn’t exist');
 }
